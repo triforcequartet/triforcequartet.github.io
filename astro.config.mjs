6ed47e9e93d374714340ecd9486c5d3bbce5ec40
chrisrooke <chlrooke@gmail.com>
2025-08-02 15:58:08 +0100

    Updated components, fix for build and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://triforcequartet.github.io/triforcequartet.github.io',
+  base: '/triforcequartet/', // important for subdirectory repos!
+});
