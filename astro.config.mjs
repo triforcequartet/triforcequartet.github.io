d9b170ca39c539c4d3f174a7b444d7db9cfda595
chrisrooke <chlrooke@gmail.com>
2025-08-02 16:03:17 +0100

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://triforcequartet.github.io/triforcequartet.github.io',
-  base: '/triforcequartet/', // important for subdirectory repos!
+  base: '/',
 });
